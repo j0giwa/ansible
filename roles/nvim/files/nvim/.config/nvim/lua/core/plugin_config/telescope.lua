e1d08779efae6ca6b8d6473b570e47d5e91dcf38
Jonas Schwind <77389555+j0giwa@users.noreply.github.com>
2024-02-08 14:57:15 +0100

    Update telescope exclusions

--- roles/nvim/files/nvim/.config/nvim/lua/core/plugin_config/telescope.lua
+++ roles/nvim/files/nvim/.config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,3 +1,30 @@
+local telescope = require('telescope')
+telescope.setup{
+        defaults = {
+                prompt_prefix = "❯ ",
+                selection_caret = "❯ ",
+                file_ignore_patterns = {
+                        "node%_modules/.*",
+                        "target/.*",
+                        ".git/.*",
+                        ".cache",
+                        "*.o",
+                        "*.a",
+                        "*.out",
+                        "*.class",
+                        "*.pdf",
+                        "*.mkv",
+                        "*.mp4",
+                        "*.zip",
+                        "%.env",
+                        "yarn.lock",
+                        "package%-lock.json",
+                        "lazy%-lock.json",
+                        "init.sql",
+                },
+        },
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
