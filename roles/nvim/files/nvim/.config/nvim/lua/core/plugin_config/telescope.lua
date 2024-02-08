d08913df29fca88b0a326afe8f4574917b6c9ade
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-02-08 21:41:58 +0100

    Fixed lombok setup

--- roles/nvim/files/nvim/.config/nvim/lua/core/plugin_config/telescope.lua
+++ roles/nvim/files/nvim/.config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,31 +1,33 @@
 local telescope = require('telescope')
-telescope.setup{
-        defaults = {
-                prompt_prefix = "❯ ",
-                selection_caret = "❯ ",
-                file_ignore_patterns = {
-                        "node%_modules/.*",
-                        "target/.*",
-                        ".git/.*",
-                        ".cache",
-                        "*.o",
-                        "*.a",
-                        "*.out",
-                        "*.class",
-                        "*.pdf",
-                        "*.mkv",
-                        "*.mp4",
-                        "*.zip",
-                        "%.env",
-                        "yarn.lock",
-                        "package%-lock.json",
-                        "lazy%-lock.json",
-                        "init.sql",
-                },
-        },
-}
-
 local builtin = require('telescope.builtin')
+
+telescope.setup({
+  defaults = {
+    prompt_prefix = "❯ ",
+    selection_caret = "❯ ",
+    file_ignore_patterns = {
+      "node%_modules/.*",
+      "%.env",
+      "yarn.lock",
+      "package%-lock.json",
+      "lazy%-lock.json",
+      "init.sql",
+      "target/.*",
+      ".git/.*",
+      ".cache",
+      "*.o",
+      "*.a",
+      "*.out",
+      "*.class",
+      "*.pdf",
+      "*.mkv",
+      "*.mp4",
+      "*.zip",
+      "%.env",
+   },
+  },
+})
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
