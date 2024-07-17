be1be6762bdcb9cab18322fa06e072d70e6722c5
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-07-17 17:20:00 +0200

    Reformating

--- roles/nvim/files/nvim/.config/nvim/lua/jogiwa/init.lua
+++ roles/nvim/files/nvim/.config/nvim/lua/jogiwa/init.lua
@@ -9,20 +9,20 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+        timeout = 40,
+      })
+  end,
 })
 
 autocmd({"BufWritePre"}, {
-    group = JogiwaGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+  group = JogiwaGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
 
 autocmd('LspAttach', {
