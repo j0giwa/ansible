1f3ebdbeeb3ab1c767d03b494849a5590a4e37c6
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-07-17 22:57:30 +0200

    Fixed a obvious bug

--- roles/nvim/files/nvim/.config/nvim/lua/jogiwa/lazy/colors.lua
+++ roles/nvim/files/nvim/.config/nvim/lua/jogiwa/lazy/colors.lua
@@ -5,9 +5,9 @@ function ColorMyPencils(color)
   vim.cmd.colorscheme(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalNc", { bg = "none" })
-  vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
+  --vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
   --vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
 end
 
